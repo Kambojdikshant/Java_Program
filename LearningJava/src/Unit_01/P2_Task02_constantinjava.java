8ffdf608c2321e92c735165644560f4887988da5
DELL <DELL@DESKTOP-R2ONIKV>
2022-03-25 15:40:34 +0530

    task2

---
+++ LearningJava/src/Unit_01/P2_Task02_constantinjava.java
@@ -0,0 +1,13 @@
+package Unit_01;
+/* constant never change once a value is assigned 
+ * private : Inside the class only 
+ * protected : class + package 
+ * */
+public class P2_Task02_constantinjava {
+
+	public static void main(String[] args) {
+		
+         System.out.println("int hte ");
+	}
+
+}
