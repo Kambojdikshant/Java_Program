bb90538732459005328d297eebfc199331f12e0d
DELL <DELL@DESKTOP-R2ONIKV>
2022-03-25 15:52:55 +0530

    task

--- LearningJava/src/Unit_01/P2_Task02_constantinjava.java
+++ LearningJava/src/Unit_01/P2_Task02_constantinjava.java
@@ -2,12 +2,30 @@ package Unit_01;
 /* constant never change once a value is assigned 
  * private : Inside the class only 
  * protected : class + package 
+ * public : class + within package + outside the package 
+ * Default : No keyword, private-private{Not outside the package}
  * */
 public class P2_Task02_constantinjava {
-
+    final double pi  = 3.14 ; 
 	public static void main(String[] args) {
 		
-         System.out.println("int hte ");
+         System.out.println("The value of Pi is "+pi);
+         main("I am learnig java") ; 
+         P2_Task02_constantinjava obj = new P2_Task02_constantinjava() ; 
+         obj.add(10, 20);
+	}
+	public static void main(String s) {
+		System.out.println(s);
+		System.out.println("The value of the pi "+pi);
+	}
+	void add(int a, int b)
+	{
+		System.out.println("The sum of two number is "+ a+b);
 	}
 
+}
+
+class Newclass
+{
+	
 }
