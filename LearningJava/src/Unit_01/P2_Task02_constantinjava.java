d6d4f4ec47e83b274635a0d2c6d853668b999e00
DELL <DELL@DESKTOP-R2ONIKV>
2022-03-25 17:38:22 +0530

    Practical2

--- LearningJava/src/Unit_01/P2_Task02_constantinjava.java
+++ LearningJava/src/Unit_01/P2_Task02_constantinjava.java
@@ -4,28 +4,34 @@ package Unit_01;
  * protected : class + package 
  * public : class + within package + outside the package 
  * Default : No keyword, private-private{Not outside the package}
- * */
+ */
+
 public class P2_Task02_constantinjava {
-    final double pi  = 3.14 ; 
+    public static final double pi = 3.14 ; 
 	public static void main(String[] args) {
 		
          System.out.println("The value of Pi is "+pi);
          main("I am learnig java") ; 
          P2_Task02_constantinjava obj = new P2_Task02_constantinjava() ; 
          obj.add(10, 20);
+         Newclass obj2 = new Newclass() ; 
+         obj2.add(25, 65);
 	}
 	public static void main(String s) {
 		System.out.println(s);
-		System.out.println("The value of the pi "+pi);
+		System.out.println("The value of the pi "+ pi);
 	}
-	void add(int a, int b)
+	public void add(int a, int b)
 	{
-		System.out.println("The sum of two number is "+ a+b);
+		System.out.println("The sum of two number is "+ (a+b));
 	}
 
 }
-
 class Newclass
 {
-	
+	public void add(int a, int b)
+	{
+		System.out.println("The sum of two number is " + (a+b));
+	}
 }
+
